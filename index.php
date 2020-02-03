f05e44c04df8cec6732ffa9fecd7e66dd8c1ed65
Antonina Lits <55576677+Foxie-lis@users.noreply.github.com>
2020-02-03 13:41:28 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,128 @@
+<html lang='en'>
+	<head>
+		<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+		<link href='css/reset.css' rel='stylesheet'>
+		<link href='css/normalize.css' rel='stylesheet'>
+		<link href='css/style.css' rel='stylesheet'>
+		<script src="https://kit.fontawesome.com/13d78327ca.js" crossorigin="anonymous"></script>
+	</head>
+	<body>
+	<div class='wrapper'>
+			<header>
+			<div class='header-top'>
+				<div id='logo'>UIBrush</div>
+				<nav class='nav-top'>
+					<ul>
+						<li><a href="#">home</a></li>
+						<li><a href="#">about us</a></li>
+						<li><a href="#">portfolio</a></li>
+						<li><a href="#">services</a></li>
+						<li><a href="#">contact us</a></li>
+					</ul>
+				</nav>
+			</div>
+				<div id='promo-text'>
+					<p>the ultimate collection of free psd templates!!</p>
+					<input type='submit' class='button' value='download'>
+				</div>
+			</header>
+			<main>
+				<section id='about-us'>
+					<h1 class='section-name'>About us</h1>
+					<p class='section-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus lorem, ornare ac felis vel.</p>
+					<div id='about-us-content'>
+						<img src="css/img/car.jpg">
+							<div class='section-content'>
+								<h2 class='section-content-head'><span class='border-bot'>lorem ipsum</span> dolore sit amet adipiscing elit</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus lorem, ornare ac felis vel, tempus consectetur nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras posuere nunc ac ex sollicitudin rhoncus. Sed nec semper orci. Nam odio nisl, sollicitudin sit amet quam ac, placerat semper augue. Curabitur quis placerat sem. Praesent tristique, leo non dapibus posuere, ex lectus gravida sem, pretium porta neque sapien in neque. Aenean a placerat libero, id mattis ex.</p>
+								<button></button>
+							</div>
+						</div>
+				</section>
+				<section id='portfolio'>
+					<h1 class='section-name'>About us</h1>
+					<p class='section-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus lorem, ornare ac felis vel, tempus consectetur nibh.</p>
+					<div class='portfolio-gallery'>
+						<div class='gallery-item'>
+							<img src='/'>
+							<h6 class='gallery-item-name'>project one</h6>
+							<p class='gallery-item-type'>web design</p>
+						</div>
+						<div class='gallery-item'>
+							<img src='/'>
+							<h6 class='gallery-item-name'>project one</h6>
+							<p class='gallery-item-type'>web design</p>
+						</div>
+						<div class='gallery-item'>
+							<img src='/'>
+							<h6 class='gallery-item-name'>project one</h6>
+							<p class='gallery-item-type'>web design</p>
+						</div>
+						<div class='gallery-item'>
+							<img src='/'>
+							<h6 class='gallery-item-name'>project one</h6>
+							<p class='gallery-item-type'>web design</p>
+						</div>
+						<div class='gallery-item'>
+							<img src='/'>
+							<h6 class='gallery-item-name'>project one</h6>
+							<p class='gallery-item-type'>web design</p>
+						</div>
+						<div class='gallery-item'>
+							<img src='/'>
+							<h6 class='gallery-item-name'>project one</h6>
+							<p class='gallery-item-type'>web design</p>
+						</div>
+					</div>
+				</section>
+				<section id='services'>
+					<h1 class='section-name'>About us</h1>
+					<p class='section-desc'>Lorem ipsum dolor sit amet</p>
+					<div class='services-item'>
+						<i class="fas fa-camera"></i>
+						<h3 class='services-item-name'>photography</h3>
+						<p class='services-item-desc'></p>
+						<button></button>
+					</div>
+					<div class='services-item'>
+						<i class="fas fa-camera"></i>
+						<h3 class='services-item-name'>photography</h3>
+						<p class='services-item-desc'></p>
+						<button></button>
+					</div>
+					<div class='services-item'>
+						<i class="fas fa-camera"></i>
+						<h3 class='services-item-name'>photography</h3>
+						<p class='services-item-desc'></p>
+						<button></button>
+					</div>
+					<div class='services-item'>
+						<i class="fas fa-camera"></i>
+						<h3 class='services-item-name'>photography</h3>
+						<p class='services-item-desc'></p>
+						<button></button>
+					</div>
+				</section>
+				<section id='contact-us'>
+					<h1 class='section-name'>About us</h1>
+					<p class='section-desc'>Lorem ipsum dolor sit amet</p>
+					<div id='contact-us-content'>
+						<h2 class='section-content-head'>lorem ipsum dolore sit amet adipiscing elit</h2>
+						<p class='section-content'>contentcontentcpntentcontent</p>
+						<h4>Address</h4>
+						<p>1931 Dawson Drive,<br>15th Avenue<br>Little Rock, AR72211</p>
+					</div>
+					<form id='contact-form'>
+						<input type='text' placeholder='Name'>
+						<input type='text' placeholder='Name'>
+						<input type='text' placeholder='Name'>
+						<textarea></textarea>
+						<input type='submit' value='submit'>
+						
+					</form>
+				</section>
+			</main>
+			<footer></footer>
+		</div>
+	</body>
+</html>
