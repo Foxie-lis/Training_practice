e519fc7fe2cacea23b740384c5d772c3a8a96dbc
Antonina Lits <55576677+Foxie-lis@users.noreply.github.com>
2020-02-17 06:18:19 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -33,96 +33,118 @@
 					<div id='about-us-content'>
 						<img src="css/img/car.jpg">
 							<div class='section-content'>
-								<h2 class='section-content-head'><span class='border-bot'>lorem ipsum</span> dolore sit amet adipiscing elit</h2>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus lorem, ornare ac felis vel, tempus consectetur nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras posuere nunc ac ex sollicitudin rhoncus. Sed nec semper orci. Nam odio nisl, sollicitudin sit amet quam ac, placerat semper augue. Curabitur quis placerat sem. Praesent tristique, leo non dapibus posuere, ex lectus gravida sem, pretium porta neque sapien in neque. Aenean a placerat libero, id mattis ex.</p>
-								<button></button>
+								<h2 class='section-content-head'><span class='border-bot'>lorem ipsum</span>dolore sit amet adipiscing elit</h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus lorem, ornare ac felis vel, tempus consectetur nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.<br><br> Cras posuere nunc ac ex sollicitudin rhoncus. Sed nec semper orci. Nam odio nisl, sollicitudin sit amet quam ac, placerat semper augue.</p>
+								<input type='submit' class='button' value='get more'>
 							</div>
-						</div>
+					</div>
 				</section>
 				<section id='portfolio'>
-					<h1 class='section-name'>About us</h1>
+					<h1 class='section-name'>Portfolio</h1>
 					<p class='section-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus lorem, ornare ac felis vel, tempus consectetur nibh.</p>
 					<div class='portfolio-gallery'>
 						<div class='gallery-item'>
-							<img src='/'>
-							<h6 class='gallery-item-name'>project one</h6>
-							<p class='gallery-item-type'>web design</p>
+							<div class="img-hover"><input type="submit" value="read more"></div>
+							<img src='css/img/placeholder.jpg'>
+							<div class='item-desc'>
+								<h6 class='gallery-item-name'>project one</h6>
+								<p class='gallery-item-type'>web design</p>
+							</div>
 						</div>
 						<div class='gallery-item'>
-							<img src='/'>
-							<h6 class='gallery-item-name'>project one</h6>
-							<p class='gallery-item-type'>web design</p>
+							<div class="img-hover"><input type="submit" value="read more"></div>
+							<img src='css/img/placeholder.jpg'>
+							<div class='item-desc'>
+								<h6 class='gallery-item-name'>project one</h6>
+								<p class='gallery-item-type'>web design</p>
+							</div>
 						</div>
 						<div class='gallery-item'>
-							<img src='/'>
-							<h6 class='gallery-item-name'>project one</h6>
-							<p class='gallery-item-type'>web design</p>
+							<div class="img-hover"><input type="submit" value="read more"></div>
+							<img src='css/img/placeholder.jpg'>
+							<div class='item-desc'>
+								<h6 class='gallery-item-name'>project one</h6>
+								<p class='gallery-item-type'>web design</p>
+							</div>
 						</div>
 						<div class='gallery-item'>
-							<img src='/'>
-							<h6 class='gallery-item-name'>project one</h6>
-							<p class='gallery-item-type'>web design</p>
+							<div class="img-hover"><input type="submit" value="read more"></div>
+							<img src='css/img/placeholder.jpg'>
+							<div class='item-desc'>
+								<h6 class='gallery-item-name'>project one</h6>
+								<p class='gallery-item-type'>web design</p>
+							</div>
 						</div>
 						<div class='gallery-item'>
-							<img src='/'>
-							<h6 class='gallery-item-name'>project one</h6>
-							<p class='gallery-item-type'>web design</p>
+							<div class="img-hover"><input type="submit" value="read more"></div>
+							<img src='css/img/placeholder.jpg'>
+							<div class='item-desc'>
+								<h6 class='gallery-item-name'>project one</h6>
+								<p class='gallery-item-type'>web design</p>
+							</div>
 						</div>
 						<div class='gallery-item'>
-							<img src='/'>
-							<h6 class='gallery-item-name'>project one</h6>
-							<p class='gallery-item-type'>web design</p>
+							<div class="img-hover"><input type="submit" value="read more"></div>
+							<img src='css/img/placeholder.jpg'>
+							<div class='item-desc'>
+								<h6 class='gallery-item-name'>project one</h6>
+								<p class='gallery-item-type'>web design</p>
+							</div>
 						</div>
 					</div>
 				</section>
 				<section id='services'>
-					<h1 class='section-name'>About us</h1>
-					<p class='section-desc'>Lorem ipsum dolor sit amet</p>
+					<h1 class='section-name'>services</h1>
+					<p class='section-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus lorem, ornare ac felis vel.</p>
+					<div class='portfolio-gallery'>
 					<div class='services-item'>
 						<i class="fas fa-camera"></i>
 						<h3 class='services-item-name'>photography</h3>
-						<p class='services-item-desc'></p>
-						<button></button>
+						<p class='services-item-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus</p>
+						<button>Read more</button>
 					</div>
 					<div class='services-item'>
 						<i class="fas fa-camera"></i>
 						<h3 class='services-item-name'>photography</h3>
-						<p class='services-item-desc'></p>
-						<button></button>
+						<p class='services-item-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus</p>
+						<button>Read more</button>
 					</div>
 					<div class='services-item'>
 						<i class="fas fa-camera"></i>
 						<h3 class='services-item-name'>photography</h3>
-						<p class='services-item-desc'></p>
-						<button></button>
+						<p class='services-item-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus</p>
+						<button>Read more</button>
 					</div>
 					<div class='services-item'>
 						<i class="fas fa-camera"></i>
 						<h3 class='services-item-name'>photography</h3>
-						<p class='services-item-desc'></p>
-						<button></button>
+						<p class='services-item-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus</p>
+						<button>Read more</button>
+					</div>
 					</div>
 				</section>
 				<section id='contact-us'>
 					<h1 class='section-name'>About us</h1>
-					<p class='section-desc'>Lorem ipsum dolor sit amet</p>
-					<div id='contact-us-content'>
-						<h2 class='section-content-head'>lorem ipsum dolore sit amet adipiscing elit</h2>
-						<p class='section-content'>contentcontentcpntentcontent</p>
-						<h4>Address</h4>
-						<p>1931 Dawson Drive,<br>15th Avenue<br>Little Rock, AR72211</p>
+					<p class='section-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus lorem, ornare ac felis vel.</p>
+					<div class="contact-us-flex">
+						<div id='contact-us-content'>
+							<h2 class='section-content-head'>contact info</h2>
+							<p class='section-content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus lorem, ornare ac felis vel, tempus consectetur nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+							<h4>Address</h4>
+							<p>1931 Dawson Drive,<br>15th Avenue<br>Little Rock, AR72211</p>
+						</div>
+						<form id='contact-form'>
+							<input type='text' placeholder='Name' class='input-name' style='margin-right: 17px;'>
+							<input type='text' placeholder='Name' class='input-name'>
+							<input type='text' placeholder='Name' class='input-topic'>
+							<textarea></textarea>
+							<input type='submit' value='submit' class='button'>
+							
+						</form>
 					</div>
-					<form id='contact-form'>
-						<input type='text' placeholder='Name'>
-						<input type='text' placeholder='Name'>
-						<input type='text' placeholder='Name'>
-						<textarea></textarea>
-						<input type='submit' value='submit'>
-						
-					</form>
 				</section>
 			</main>
-			<footer></footer>
+			<footer>Copyright &copy 2014 UIBrush.</footer>
 		</div>
 	</body>
 </html>
